60cc7e051b8189cdd8a85ed3f674cc3d72a2c48a
Kyle Nguyen <67439231+Kiezroy@users.noreply.github.com>
2023-06-14 12:25:24 -0700

    Add files via upload

---
+++ Lab2/lab2.c
@@ -0,0 +1,17 @@
+/*-------------------*/
+/*Kyle Nguyen        */
+/*Lab 2              */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    printf("\nLab 2. \n\n");
+    printf("Hello World!\n");
+    printf("Hi, Kyle Nguyen. \n");
+    printf("\nThese Things We Do, That Others May Live\n");
+    printf("- United States Air Force Pararescue\n\n");
+
+    return EXIT_SUCCESS;
+}
